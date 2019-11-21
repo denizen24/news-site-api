02c1cd32656c0e36742736f0691229cd166a29b3
Vladimirov Denis <akakiy813@yandex.ru>
2019-11-21 08:36:53 +0300

    fix get errors in login

--- controllers/login.js
+++ controllers/login.js
@@ -1,13 +1,17 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user').default;
+const NotAuthError = require('../errors/not-auth');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-module.exports.login = (req, res) => {
+module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
+      if (!user) {
+        throw new NotAuthError('Ошибка авторизации');
+      }
       const token = jwt.sign(
         { _id: user._id },
         (NODE_ENV === 'production' ? JWT_SECRET : 'secret-key'),
@@ -22,9 +26,5 @@ module.exports.login = (req, res) => {
         .send({ message: 'авторизация - OK' })
         .end();
     })
-    .catch((err) => {
-      res
-        .status(401)
-        .send({ message: err.message });
-    });
+    .catch(next);
 };
