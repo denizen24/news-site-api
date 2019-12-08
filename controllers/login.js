3f09571dbbdc7d502c4451991c27d10ba7062ae1
Vladimirov Denis <akakiy813@yandex.ru>
2019-12-08 17:53:14 +0300

    add cors

--- controllers/login.js
+++ controllers/login.js
@@ -1,17 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user').default;
-const NotAuthError = require('../errors/not-auth');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-module.exports.login = (req, res, next) => {
+module.exports.login = (req, res) => {
   const { email, password } = req.body;
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      if (!user) {
-        throw new NotAuthError('Ошибка авторизации');
-      }
       const token = jwt.sign(
         { _id: user._id },
         (NODE_ENV === 'production' ? JWT_SECRET : 'secret-key'),
@@ -26,5 +22,9 @@ module.exports.login = (req, res, next) => {
         .send({ message: 'авторизация - OK' })
         .end();
     })
-    .catch(next);
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
+    });
 };
